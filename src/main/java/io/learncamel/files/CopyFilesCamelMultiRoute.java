ad5d20ba1cc30b826d1cdeb272d1003b74c2f4ad
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-06 20:07:25 +0530

    Camel MultiRoute

---
+++ src/main/java/io/learncamel/files/CopyFilesCamelMultiRoute.java
@@ -0,0 +1,28 @@
+package io.learncamel.files;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CopyFilesCamelMultiRoute {
+	
+	public static void main(String[] args) {
+		CamelContext camelContext = new DefaultCamelContext();
+		try {
+			camelContext.addRoutes(new RouteBuilder() {
+				@Override
+				public void configure() throws Exception {
+					from("file:data/input?noop=true")
+					.to("log:?level=info&showBody=true&showHeaders=true")
+					.to("file:data/output");
+				}
+			});
+			camelContext.start();
+			Thread.sleep(5000);
+			camelContext.stop();
+		} catch(Exception e) {
+			System.out.println("Exception is : " + e);
+			e.printStackTrace();
+		}
+	}
+}
